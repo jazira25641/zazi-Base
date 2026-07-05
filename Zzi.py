20d7b82a243c460ba090d4a73141f993cadd63b2
jazira25641 <137087666+jazira25641@users.noreply.github.com>
2026-07-05 07:03:51 +0000

    Update Zzi.py

--- Zzi.py
+++ Zzi.py
@@ -59,3 +59,4 @@ const total = Number(params.amount)
 return (total * 0.01).toFixed(6) // Example: 1% product fee
 },
  resolveFeeRecipientAddress: (feePayoutChain) => {
+ return feePayoutChain.type === "solana"
